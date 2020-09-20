1c640c38a585e2c983a64c38a1e7190ce053fdb5
= <vnitusingh@yahoo.com>
2020-09-20 14:26:42 -0400

    Scraping code for Mars data

---
+++ app.py
@@ -0,0 +1,28 @@
+# Import Dependencies
+
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+# Initialize the flask app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    print(mars_data)
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful!"
+
+if __name__ == "__main__":
+    app.run()    
